7054e8bf751a313255045c1e1f1e83abdb2a1869
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-06-23 17:56:37 +1000

    comments input into HTML CSS and Javascript

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-
+// DOM function 
 $("document").ready(function() {
 
+// todays Date   
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
+
+// Current Time
 var time = moment();
 $("#currentTime").text(time.format("hh:mm:ss"));
 
-
-
+// current hour Console logged 
 var currentHour = moment().hours();
-$("#currentHour").text(time.format("h"));
+console.log(currentHour)
 
+// Element identifies what has been entered in each "textarea", and then applies a "function" to ".each" 
 inputEl = $("textarea").each(function(){
     var textEL = $(this).attr("id");
     console.log(textEL);
-
+//if textEl hour is less than "currentTime" add the "past" css stylings   
     if ( textEL < currentTime) {
         $(this).addClass("past");
-
+//if textEl hour is equal to "currentTime" add the "present" css stylings
     } else if ( textEL == currentTime) {
         $(this).addClass("present");
-
+//if textEl hour is more than "currentTime" add the "future" css stylings
     } else if ( textEL > currentTime) {
         $(this).addClass("future");
     }
@@ -32,14 +35,17 @@ inputEl = $("textarea").each(function(){
     
 });
 
-
+// on the click of the .saveBtn this function will run
 $(".saveBtn").on("click", function(){
+    //function sets the text and value of each save button to a value
     var hour = $(this).siblings(".hour").text();
     var description = $(this).siblings(".description").val();
-
+//stores the variable value to the local storage
     localStorage.setItem(hour, description);
 })
 
+
+// Gets the time and description of each value from the local storage
 $("#9am .description").val(localStorage.getItem("9am"));
 $("#10am .description").val(localStorage.getItem("10am"));
 $("#11am .description").val(localStorage.getItem("11am"));
